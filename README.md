b91d289465fbc4622f1f7cf177a825e2ee735bb7
inspira <inspira@labsoft.local>
2024-04-17 16:52:54 -0300

    finalizacao ingressos

---
+++ README.md
@@ -0,0 +1,8 @@
+# Prova final - Reserva de ingressos
+
+## PCS3853 - Laboratório de Engenharia de Software II (2024)
+
+Aluno: Luiz Guilherme Budeu
+NUSP: 11821639
+
+# RODAR NA PORTA 8000
--- ingresso/settings.py
+++ ingresso/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2ggw-6-_f(z!=ad3rj9b1b38(kpo1o2__u=a+#qa&wxs*ev_*j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
